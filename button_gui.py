0eafc6f21ca066844a0126bcb3b6eb35be7f68fe
Garrett <gjohnson2718@gmail.com>
2019-07-23 18:06:47 +0200

    Well, I guess that actually planning software before writing it makes things a lot easier. It works, though it does allow me to select the whitespace around the sprite.

--- button_gui.py
+++ button_gui.py
@@ -37,15 +37,35 @@ class MyGame(arcade.Window):
         self.button_y = WINDOW_HEIGHT // 2
 
         #  TODO appending button sprite to sprite list
+        self.button_sprite_list = arcade.SpriteList()
+        self.button_sprite = arcade.Sprite(r"C:\Users\Garrett\Pictures\py_sprites\blue_button.jpg")
+        self.button_sprite_list.append(self.button_sprite)
+
+        # Setting the button in the proper place
+        self.button_sprite.center_x = self.button_x
+        self.button_sprite.center_y = self.button_y
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text("This button has been pushed " + str(self.times_pressed) + " times.",
                          200, 500, arcade.color.BLACK)
-        # TODO add in draw functions for sprite lists
+        self.button_sprite_list.draw()
 
-    # TODO override update()
+    def update(self, delta_time: float):
+        self.button_sprite_list.update()
 
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        # Defining variables for button location
+        button_x_lower = self.button_sprite.center_x - (self.button_sprite.width / 2)
+        button_x_upper = self.button_sprite.center_x + (self.button_sprite.width / 2)
+
+        button_y_lower = self.button_sprite.center_y - (self.button_sprite.height / 2)
+        button_y_upper = self.button_sprite.center_y + (self.button_sprite.height / 2)
+
+        on_button_location = button_x_lower < x < button_x_upper and button_y_lower < button_y_upper
+
+        if on_button_location and button == arcade.MOUSE_BUTTON_LEFT:
+            self.times_pressed += 1
 
 def main():
     game = MyGame(arcade.color.WHITE)
